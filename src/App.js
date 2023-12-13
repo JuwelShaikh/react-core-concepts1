c3830a7dc6e2b581e36c88e0add2f8bfbc216297
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-13 16:39:43 +0600

    Design added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <Counter></Counter>
     </div>
   );
+}
+
+function Counter (){
+  return(
+    <div className='count'>
+      <h3>Count: </h3>
+      <button>Increase</button>
+    </div>
+  )
 }
 
 export default App;
